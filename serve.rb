5261bcaf74642f6ff55deafaee873eefca3271a1
Naoto SHINGAKI <nao.sora+github@gmail.com>
2012-08-02 09:43:36 +0900

    first init

---
+++ serve.rb
@@ -0,0 +1,121 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'sinatra/contrib'
+require 'erb'
+require 'json'
+require 'RMagick'
+
+namespace '/friends' do
+
+  get '/' do
+    erb :index
+  end
+
+  get '/videos' do
+    {video: Dir.glob("public/friends/video/**/*").map { |f| f.gsub(/^public\/friends\/video\//,'') }.sort }.to_json
+  end
+
+  get '/pictures' do
+    {picture: Dir.glob("public/friends/original/picture/**/*").map { |f| f.gsub(/^public\/friends\/original\/picture\//,'')}.sort }.to_json
+  end
+
+  get '/tumb/:filename' do
+    path = "public/tumb/#{params[:filename]}"
+    Magick::ImageList.new("public/friends/original/picture/#{params[:filename]}").resize_to_fill(240,240).write(path) unless File.exists?(path)
+    open(path).read
+  end
+
+  get '/pic/:filename' do
+    @filename = params[:filename]
+    erb :pic
+  end
+
+  get '/pic/L/:filename' do
+    path = "public/L/#{params[:filename]}"
+    Magick::ImageList.new("public/friends/original/picture/#{params[:filename]}").resize_to_fit(1024,960).write(path) unless File.exists?(path)
+    open(path).read
+  end
+
+end
+
+__END__
+
+@@ pic
+<html>
+  <head>
+    <title><e</title>
+    <link rel="stylesheet" href="../css/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="../css/print.css" type="text/css" media="print">
+    <!--[if IE]>
+      <link rel="stylesheet" href="../css/ie.css" type="text/css" media="screen, projection">
+    <![endif]-->
+    <link rel="stylesheet" href="../css/style.css" type="text/css" media="screen, projection">
+  </head>
+  <body>
+  <div class="container">
+      <h1 id="title" class="span-16">Album</h1>
+      <div class="span-8">
+        <a id="video" href="#" class="button">VIDEO</a>
+        <a id="picture" href="#" class="button">PICTURE</a>
+        <a id="original" href="../original/picture/<%= @filename %>" class="button">ORIGINAL</a>
+      </div>
+  </div>
+  <div class="container">
+    <div id="main" class="span-24">
+      <img src="./L/<%= @filename %>"/>
+    </div>
+  </div>
+  </body>
+</html>
+
+@@ index
+<html>
+  <head>
+    <title><e</title>
+    <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="css/print.css" type="text/css" media="print">
+    <!--[if IE]>
+      <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection">
+    <![endif]-->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection">
+    <script src="js/jquery-1.7.2.min.js"></script>
+    <script>
+      $(document).ready(function(){
+        $('#picture').click(function(){
+          $.getJSON("./pictures", function(data){
+            $("#main").html("");
+            $.each(data.picture, function(){
+              $("#main").append("<div class=\"span-6\"><a href=\"./pic/" + this + "\" target=\"_blank\"><img src=\"./tumb/" + this +"\" /></a></span>");
+            });
+          });
+        });
+        $('#video').click(function(){
+          $.getJSON("./videos", function(data){
+            $("#main").html("");
+            $.each(data.video,  function(){
+              $("#main").append("<a class=\"button video\">" + this + "</a");
+            });
+            $("#main").append("<div id=\"player\" class=\"span-24\"></div>");
+            $(".video").click(function(){
+              $("#player").html("<video src=\"./video/" + $(this).text() + "\" controls></vide>");
+            });
+          });
+        });
+      });
+    </script>
+  </head>
+  <body>
+  <div class="container">
+      <h1 id="title" class="span-16">Album</h1>
+      <div class="span-8">
+        <a id="video" href="#" class="button">VIDEO</a>
+        <a id="picture" href="#" class="button">PICTURE</a>
+      </div>
+  </div>
+  <div class="container">
+    <div id="main" class="span-24">
+    </div>
+  </div>
+  </body>
+</html>
